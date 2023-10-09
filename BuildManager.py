ab4296af1b5c87a30dc03cdd522feadb7cc8c9ae
rosrunner <luca.alfano@uni-a.de>
2023-10-09 16:22:53 +0200

    Added language

--- BuildManager.py
+++ BuildManager.py
@@ -25,3 +25,26 @@ class BuildManager(AbstractVirtualCapability):
 
     def GetNextBlockPosition(self, params: dict):
         return params
+
+if __name__ == '__main__':
+    # Needed for properly closing when process is being stopped with SIGTERM signal
+    def handler(signum, frame):
+        print("[Main] Received SIGTERM signal")
+        listener.kill()
+        quit(1)
+
+
+    try:
+        port = None
+        if len(sys.argv[1:]) > 0:
+            port = int(sys.argv[1])
+        server = VirtualCapabilityServer(port)
+        listener = BuildManager(server)
+        listener.start()
+        signal.signal(signal.SIGTERM, handler)
+        listener.join()
+    # Needed for properly closing, when program is being stopped wit a Keyboard Interrupt
+    except KeyboardInterrupt:
+        print("[Main] Received KeyboardInterrupt")
+        server.kill()
+        listener.kill()
