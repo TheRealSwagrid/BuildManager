be86d4503219db1a80b85260e335cb5e89e069cc
rosrunner <luca.alfano@uni-a.de>
2023-11-29 17:23:32 +0100

    added wall rotation

--- BuildManager.py
+++ BuildManager.py
@@ -78,8 +78,8 @@ class BuildManager(AbstractVirtualCapability):
         walls = self.GetWalls(params)
         points = []
         for wall in walls["ListOfPoints"]:
-            q = quaternion.from_float_array(np.append(np.dot(np.array([0., 0., 1.]), np.array(wall[:3])),
-                                                      np.cross(np.array([0., 0., 1.]), np.array(wall[:3]))))
+            q = quaternion.from_float_array(np.append(np.dot(np.array([0., 1., 0.]), np.array(wall[:3])),
+                                                      np.cross(np.array([0., 1., 0.]), np.array(wall[:3]))))
 
             points.append((np.array(wall[:3]) * wall[3]).tolist() + quaternion.as_float_array(q).tolist())
         return {"ListOfPoints": points}
